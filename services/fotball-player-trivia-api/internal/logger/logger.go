1a29821bc6e43f9697e59e8eada029fe38e2c99a
Oskar Elvkull <oskar.elvkull@tibber.com>
2022-10-27 14:42:56 +0200

    changed from file-logging to console-loggin

--- services/fotball-player-trivia-api/internal/logger/logger.go
+++ services/fotball-player-trivia-api/internal/logger/logger.go
@@ -12,13 +12,7 @@ var (
 )
 
 func init() {
-	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		log.Fatal(err)
-		panic("could not initialize logger.")
-	}
-
-	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	InfoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
